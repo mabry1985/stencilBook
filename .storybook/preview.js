a42c2664d489abe53353273150104abc303e5daf
Josh Mabry <jmabry@phase2technology>
2021-04-03 19:46:21 -0700

    add script for automating stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,21 @@
-import { defineCustomElements } from '../dist/esm/loader';
+import { configure } from '@storybook/html';
+import buildStencilStories from './stories/automatedStories';
 
-defineCustomElements();
+const loader = require('../loader/index.cjs.js');
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+const COLLECTIONS = [
+  {
+    name: 'My Components',
+    componentsCtx: require.context('../dist/collection', true, /\/components\/([^/]+)\/\1\.js$/),
+    storiesCtx: require.context('../src/', true, /\.stories\.tsx$/),
+  },
+];
+
+function loadStories() {
+  loader.defineCustomElements(window);
+  COLLECTIONS.forEach(({ name, componentsCtx, storiesCtx }) => {
+    buildStencilStories(name, componentsCtx, storiesCtx);
+  });
 }
+
+configure(loadStories, module);
