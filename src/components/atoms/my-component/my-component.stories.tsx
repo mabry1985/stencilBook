74c8384825b1aaee8c5e4ca0ed738f6daa1d4a46
Josh Mabry <jmabry@phase2technology>
2021-04-03 19:45:28 -0700

    move my-component into atoms

---
+++ src/components/atoms/my-component/my-component.stories.tsx
@@ -0,0 +1,12 @@
+import readme from './readme.md';
+
+export default {
+  name: 'Atoms',
+  title: 'MyComponent',
+  /**
+   * Notes is an optional string which will be displayed in the "Notes"
+   * tab for your component. It is recommended to use the generated readme,
+   * however any string should suffice. This supports markdown.
+   */
+  notes: readme,
+};
