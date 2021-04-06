107ab48976afc4b8fb77bc9da8acd7b9305cab7f
Josh Mabry <jmabry@phase2technology>
2021-04-05 21:47:50 -0700

    remove reference to defineCustomElements

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,11 @@
-import { defineCustomElements } from '../dist/esm/loader';
-
-defineCustomElements();
-
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  docs: {
+    extractComponentDescription: (component, { notes }) => {
+      if (notes) {
+        return typeof notes === 'string' ? notes : notes.markdown || notes.text;
+      }
+      return null;
+    },
+  },
+};
