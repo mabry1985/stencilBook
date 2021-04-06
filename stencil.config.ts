f137c0a9236ba2356d41a0cf15354a524cec94e8
Josh Mabry <jmabry@phase2technology>
2021-04-05 21:48:29 -0700

    update stencil config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,21 +1,41 @@
 import { Config } from '@stencil/core';
 
+const { name, distDirs } = require('./package.json');
+
 export const config: Config = {
-  namespace: 'stencilbook',
+  namespace: name,
+  buildEs5: false,
+  taskQueue: 'async',
+  plugins: [],
+  globalStyle: 'src/global.css',
+  devServer: {
+    reloadStrategy: 'hmr',
+    openBrowser: false,
+  },
   outputTargets: [
+    // creates /dist dir
     {
       type: 'dist',
-      esmLoaderPath: '../loader',
+      dir: distDirs.stencil,
+      // copy: [
+      //   // copy fonts into static for storybook and stencil build
+      //   { src: 'fonts' },
+      // ],
     },
+    // one file in es6
     {
       type: 'dist-custom-elements-bundle',
+      dir: distDirs.stencil,
     },
+    // creates readme.md for components
     {
       type: 'docs-readme',
+      dir: distDirs.stencil,
     },
+    // create components(.d.ts|json) into dist
     {
-      type: 'www',
-      serviceWorker: null, // disable service workers
+      type: 'docs-json',
+      file: `${distDirs.stencil}/components.json`,
     },
   ],
 };
